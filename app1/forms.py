62a53a99827dd138f99c77be1227e97a2e778c2e
Chuyi <chutoxibe@gmail.com>
2023-12-08 22:19:25 -0700

    Add basic functions for the moment

---
+++ app1/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name in self.fields:
+            self.fields[field_name].help_text = ''
+
+
--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,logout
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+
+def index(request):
+    user=request.user
+    if request.method == 'POST':
+        form=AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user=form.get_user()
+            login(request,user)
+            return redirect('home')
+        
+    elif user.is_authenticated:
+        return redirect('home')
+
+    else:
+        form=AuthenticationForm()
+    context = {
+        'form':form,
+    }
+    return render(request,'index.html',context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form=CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            login(request,user)
+            return redirect('index')
+
+    else:
+        form=CustomUserCreationForm()
+    context = {
+        'form':form,
+    }
+    return render(request,'register.html',context)
+
+def home(request):
+    return render(request,'home.html')
